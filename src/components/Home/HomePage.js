652acf33fc876db92ceda17899f9583fcafc6199
Sam Gehret <sam.gehret@gmail.com>
2018-04-17 09:44:13 -0400

    got the verification to work

--- src/components/Home/HomePage.js
+++ src/components/Home/HomePage.js
@@ -1,7 +1,31 @@
 import React, { Component } from 'react'
+import { withAuth } from '@okta/okta-react'
 import { Link } from 'react-router-dom'
 
-class Home extends Component {
+export default withAuth(class Home extends Component {
+  constructor (props) {
+    super(props)
+  }
+  async componentDidMount () {
+    var token = await this.props.auth.getAccessToken()
+    console.log(token)
+    console.log('home')
+    try {
+      const response = await fetch('http://localhost:3001/users/list', {
+        headers: {
+          Authorization: 'Bearer ' + await this.props.auth.getAccessToken()
+        }
+      })
+      const data = await response.json()
+      console.log(data)
+    } catch (err) {
+
+        // handle error as needed
+    }
+  }
+
+      // handle error as needed
+
   render () {
     return (
       <div>
@@ -11,6 +35,4 @@ class Home extends Component {
       </div>
     )
   }
-  }
-
-export default Home
+  })
--- src/components/auth/ProfilePage.js
+++ src/components/auth/ProfilePage.js
@@ -5,28 +5,54 @@ import axios from 'axios'
 export default withAuth(class ProfilePage extends React.Component {
   constructor (props) {
     super(props)
-    this.state = { user: null }
+    this.state = {
+      user: null,
+      data: null }
     this.getCurrentUser = this.getCurrentUser.bind(this)
-    this.getAUser = this.getAUser.bind(this)
-}
+    this.getUsers = this.getUsers.bind(this)
+  }
 
   async getCurrentUser () {
     this.props.auth.getUser()
       .then(user => {
         this.setState({user})
-        console.log(user)
+        // console.log(user)
       })
   }
 
-  getAUser () {
-    axios.get('https://dev-320743.oktapreview.com/api/v1/users',
-    {headers: {authorization: 'SSWS 002ml4zCt6VpTP_akzJ9jdOBWRoUw5TDG_OyjOSlMR'}})
-    .then((list) => console.log(list))
+  getUsers () {
+    console.log('suh')
   }
 
-  componentDidMount () {
+//   async componentDidMount () {
+//       console.log('suh')
+//     axios.get('https://dev-320743.oktapreview.com/api/v1/users',
+//       {
+//         headers: {
+//           Authorization: 'Bearer ' + await this.props.auth.getAccessToken()
+//         }
+//       })
+//       .then((res) => console.log(res))
+//   }
+
+  async componentDidMount () {
     this.getCurrentUser()
-    this.getAUser()
+    this.getUsers()
+    var token = await this.props.auth.getAccessToken()
+    console.log(token)
+    try {
+      const response = await fetch('http://localhost:3001/users/list', {
+        headers: {
+          Authorization: 'Bearer ' + await this.props.auth.getAccessToken()
+        }
+      })
+      const data = await response.json()
+      this.setState({ data: data })
+      console.log(data)
+    } catch (err) {
+
+        // handle error as needed
+    }
   }
 
   render () {
@@ -35,6 +61,7 @@ export default withAuth(class ProfilePage extends React.Component {
       <section className='user-profile'>
         <h1>User Profile</h1>
         <div>
+          <p>{this.state.messages}</p>
           <label>Name:</label>
           <span>{this.state.user.name}</span>
         </div>
