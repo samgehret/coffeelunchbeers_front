bfbdd86ebee5dab3b5d0dce0e19bd618f541d553
Sam Gehret <sam.gehret@gmail.com>
2018-04-20 09:14:25 -0400

    added heroku config

--- src/app.config.js
+++ src/app.config.js
@@ -3,5 +3,5 @@ export default {
   issuer: 'https://dev-320743.oktapreview.com/oauth2/auseqn9hpdtwm1aeO0h7',
   redirect_uri: window.location.origin + '/implicit/callback',
   client_id: '0oaeqew6h4dp1KLel0h7',
-  serverUrl: 'http://localhost:3001'
+  serverUrl: 'https://coffeelunchbeers.herokuapp.com'
 }
